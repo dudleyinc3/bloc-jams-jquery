6a031a4757bf229814825053b5592d1d554dd6af
dudleyinc3 <dudleyinc3@gmail.com>
2018-01-01 16:43:52 +0800

    set play/pause and next buttons

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,12 +1,33 @@
+{
+	$('button#play-pause').on('click', function(){
+		player.playPause();
+		$(this).attr('playState', player.playState);
+	});
 
-	setInterval( () => {
-     if (player.playState !== 'playing') { return; } 
-     const currentTime = player.getTime();
-     const duration = player.getDuration();
-     const percent = (currentTime / duration) * 100;
-     $('#time-control input').val(percent);   
-	}, 1000);
+	$('button#next').on('click', function(){
+		if(player.playState !== 'playing') {return;}
+
+		const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+		const nextSongIndex = currentSongIndex +1;
+
+		if(nextSongIndex >= album.songs.length) {return;}
+		const nextSong = album.songs[nextSongIndex];
+		player.playPause(nextSong);
+
+	});
+
+
 }
 
+
+/*setInterval( () => {
+   if (player.playState !== 'playing') { return; } 
+   const currentTime = player.getTime();
+   const duration = player.getDuration();
+   const percent = (currentTime / duration) * 100;
+   $('#time-control input').val(percent);   
+}, 1000);
+}
+*/
 
 
